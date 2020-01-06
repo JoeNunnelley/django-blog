5478a76126e937f8ed831c9146351131ca3a6b46
Joe Nunnelley <jcnunnelley@outlook.com>
2020-01-05 23:30:58 -0800

    initial work on django polling/blogging site

---
+++ blogging/views.py
@@ -0,0 +1,18 @@
+# blogging/views.py
+
+from django.shortcuts import render
+from django.http import Http404
+from blogging.models import Post
+
+def list_view(request):
+    context = {'polls': Post.objects.all()}
+    return render(request, 'blogging/list.html', context)
+
+def detail_view(request, blog_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
