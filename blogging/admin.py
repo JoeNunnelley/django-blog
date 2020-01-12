cee5b859b5977171fb52360a8ab1d72918e474cf
Joe Nunnelley <jcnunnelley@outlook.com>
2020-01-12 14:29:01 -0800

    reworking the admin panel to make associating categories to posts an operation done within the posts UI of the admin panel

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'author',  'created_date', 'published_date')
+    list_filter = ('created_date',)
+    date_hierarchy = 'created_date'
+    ordering = ('-created_date',)
+    fields = ('title', 'text', 'author', )
+    inlines = [ CategoryInline, ]
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
