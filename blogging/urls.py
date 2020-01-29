1f51ab7ace80867b6da2cf808545bed07b02fb5d
Joe Nunnelley <jcnunnelley@outlook.com>
2020-01-28 22:47:14 -0800

    making the blog the default view

--- blogging/urls.py
+++ blogging/urls.py
@@ -13,8 +13,9 @@ router.register(r'posts', PostViewSet)
 router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
+    path('', list_view, name='blog_root'),
     path('blog', list_view, name="blog_index"),
     path('posts/<int:post_id>/', detail_view, name="blog_detail"),
     path('latest/feed/', LatestEntriesFeed()),
-    path('', include(router.urls)),
+    path('api', include(router.urls)),
 ]
